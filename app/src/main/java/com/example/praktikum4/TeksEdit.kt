4b3510fd15f9275656780a59ca79c4c02b8cbf7f
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2025-10-31 10:24:21 +0700

    membuat elemen kedua berupa kolom bulat untuk input jenis kelamin dengan value textjk dan menggunakan onclick dengan layout row untuk jarak antara kolom bulat dan jarak antara kolom dengan teks

--- app/src/main/java/com/example/praktikum4/TeksEdit.kt
+++ app/src/main/java/com/example/praktikum4/TeksEdit.kt
@@ -65,6 +65,24 @@ fun FormDataDiri(modifier: Modifier
                 textNama = it
             }
         )
+        Row {
+
+            gender.forEach { item ->
+                Row(modifier = Modifier.selectable(
+                   selected = textJK == item,
+
+                    onClick = { textJK = item }
+                ), verticalAlignment = Alignment.CenterVertically){
+                    RadioButton(
+                        selected = textJK == item,
+
+                        onClick = {
+                            textJK = item
+                        })
+                    Text(item)
+                }
+            }
+        }
 
 
     }
