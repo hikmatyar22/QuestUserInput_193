b8f5f8dfc38e70e9316a7b11cdf12f1d6a461ba3
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2025-10-31 09:16:06 +0700

    membuat fungsi data diri untuk menyimpan variabel dan beberapa kompenen ui

---
+++ app/src/main/java/com/example/praktikum4/TeksEdit.kt
@@ -0,0 +1,15 @@
+package com.example.praktikum4
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import java.lang.reflect.Modifier
+
+@Composable
+
+fun FormDataDiri(modifier: Modifier
+){
+
+}
