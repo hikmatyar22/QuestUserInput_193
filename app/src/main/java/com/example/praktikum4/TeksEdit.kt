5e5ecd5fc7c4ce7b1b746f410a6e9e95848fd972
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2025-10-31 10:17:50 +0700

    Membuat Variabel di form data diri untuk menyimpan nilai input dan menampilakan agar muncul stirng untuk gender serta dibuat val karena datanya pasti dan tetap

--- app/src/main/java/com/example/praktikum4/TeksEdit.kt
+++ app/src/main/java/com/example/praktikum4/TeksEdit.kt
@@ -47,6 +47,6 @@ fun FormDataDiri(modifier: Modifier
     var alamat by remember { mutableStateOf("") }
     var jenis by remember { mutableStateOf("") }
 
-
+    val gender:List<String> = listOf("Laki-Laki","Peremopuan")
 
 }
